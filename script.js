a0bcf64901eb18ef0b1b080df5c2c94f3a925f4c
willdbest <nata.codedev@gmail.com>
2022-03-15 15:14:55 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const btn = document.querySelector('a')
+
+function mostarform(e){
+  e.preventDefault();
+let nome = document.querySelector('[data-nome="nome"]')
+let senha = document.querySelector('[data-senha="senha"]')
+
+document.getElementById('valorNome').innerHTML = nome.value;
+document.getElementById('valorSenha').innerText = senha.value;
+
+}
+
+btn.addEventListener('click', mostarform)
+
+
+/*********************** deletar table *****************************/
+const delet = document.querySelectorAll('.deletar-box')
+
+function deletarform() {
+let deletar = document.querySelectorAll('.table-js')
+ delet.removeChild(deletar)
+}
+
+delet.addEventListener('click' , deletarform)
+
+
 
